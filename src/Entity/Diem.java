88049640f89de320dba8e9fc5e3d91665dbe4975
Quach Truong Giang <giangquach619@gmail.com>
2022-01-03 16:22:12 +0700

    first commit

---
+++ src/Entity/Diem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+public class Diem {
+
+    private String maSV;
+    private String ngaythi;
+    private int diem;
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getNgaythi() {
+        return ngaythi;
+    }
+
+    public void setNgaythi(String ngaythi) {
+        this.ngaythi = ngaythi;
+    }
+
+    public int getDiem() {
+        return diem;
+    }
+
+    public void setDiem(int diem) {
+        this.diem = diem;
+    }
+}
---
+++ src/kiemtragiuaki/Utils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiemtragiuaki;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Utils {
+
+    public static void enableAll(JPanel p, boolean enabled) {
+        for (Component comp : p.getComponents()) {
+            if (!(comp instanceof JLabel)) {
+                comp.setEnabled(enabled);
+            }
+        }
+    }
+}
